57535b9b015615d07c52d1fbb2eb3cde4f2b4fea
yuzhenling <yuzhenling@yeah.net>
2026-02-28 13:26:43 +0800

    Refactor config.py for improved settings
    Updated configuration settings for WeChat and database.

--- app/core/config.py
+++ app/core/config.py
@@ -6,19 +6,19 @@ import secrets
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "声之宝典"
+    PROJECT_NAME: str = ""
     API_V1_STR: str = "/api/v1"
     # API_HOST: str = "https://api.shengyibaodian.com"  # API域名
-    API_HOST: str = "https://www.lianerdaren.cn"  # API域名
+
 
     APP_DIR: str = Path(__file__).resolve().parent.parent.as_posix()
 
     # PostgreSQL数据库配置
     POSTGRES_SERVER: str = "localhost"
     POSTGRES_USER: str = "postgres"
-    POSTGRES_PASSWORD: str = "123456"
-    POSTGRES_DB: str = "shengyibaodian"
-    POSTGRES_PORT: str = "5432"
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = ""
+    POSTGRES_PORT: str = ""
 
     # 数据库连接池配置
     DB_POOL_SIZE: int = 20
@@ -37,26 +37,26 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
     
     # 微信小程序配置
-    # WECHAT_APP_ID: str = "wx8db9b798acf25a4c"
-    # WECHAT_APP_SECRET: str = "4398791fcc7cf2b1b9ee11e52e3daf60"
+    # WECHAT_APP_ID: str = ""
+    # WECHAT_APP_SECRET: str = ""
 
-    WECHAT_APP_ID: str = "wxc22835bde7ea1df7"
-    WECHAT_APP_SECRET: str = "90d0293cfa9beb4d8a7cb10a24eb8dcf"
+    WECHAT_APP_ID: str = ""
+    WECHAT_APP_SECRET: str = ""
     
     # 微信支付配置
-    # WECHAT_MCH_ID: str = "1711281977"  # 商户号
-    # WECHAT_PAY_SERIAL_NO: str = "5D7B5FCEE61F97EAD3726C3DD3766683344A501F"  # 商户证书序列号
+    # WECHAT_MCH_ID: str = ""  # 商户号
+    # WECHAT_PAY_SERIAL_NO: str = ""  # 商户证书序列号
     # WECHAT_PAY_KEY: str = ""  # API v3密钥
     # WECHAT_PAY_CERT_PATH: str = APP_DIR+"/wepay/apiclient_cert.pem"  # 商户证书路径
     # WECHAT_PAY_KEY_PATH: str = APP_DIR+"/wepay/apiclient_key.pem"  # 商户私钥路径
-    # WECHAT_NOTIFY_URL: str = "http://8.152.200.145:8000/api/v1/order/wechat-notify"
+    # WECHAT_NOTIFY_URL: str = "http://:8000/api/v1/order/wechat-notify"
 
     WECHAT_MCH_ID: str = "1717958217"  # 商户号
-    WECHAT_PAY_SERIAL_NO: str = "4BBA9592E220E7031F35A5745ABC2A6FBBA89C6F"  # 商户证书序列号
+    WECHAT_PAY_SERIAL_NO: str = ""  # 商户证书序列号
     WECHAT_PAY_KEY: str = ""  # API v3密钥
     WECHAT_PAY_CERT_PATH: str = APP_DIR + "/wepay/apiclient_cert.pem"  # 商户证书路径
     WECHAT_PAY_KEY_PATH: str = APP_DIR + "/wepay/apiclient_key.pem"  # 商户私钥路径
-    WECHAT_NOTIFY_URL: str = "https://www.lianerdaren.cn/api/v1/order/wechat-notify"
+    WECHAT_NOTIFY_URL: str = "https://www..cn/api/v1/order/wechat-notify"
     
     # 音频处理配置
     AUDIO_UPLOAD_DIR: str = "uploads/audio"
@@ -64,13 +64,14 @@ class Settings(BaseSettings):
     ALLOWED_AUDIO_TYPES: list = ["audio/wav", "audio/mp3", "audio/m4a"]
     
     # DeepSeek API settings
-    DEEPSEEK_API_KEY: str = "sk-93590ae8cf3a4025a204afc8fa0f6082"
+    DEEPSEEK_API_KEY: str = "sk-"
     DEEPSEEK_API_URL: str = "https://api.deepseek.com/v1/chat/completions"
     
 
     class Config:
         env_file = ".env"
         case_sensitive = True
+
 
 
 settings = Settings() 
